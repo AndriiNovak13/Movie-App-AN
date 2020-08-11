58f98e37bd7fddd26cf8fabad3e4a438e1263428
AndriiNovak13 <AndriiNovak13@users.noreply.github.com>
2020-08-11 23:32:11 +0300

    Updates

---
+++ precache-manifest.28e966860974d081d549c6b860b5a426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a34b889e5898d5d63f81576869a3ec",
+    "url": "/Movie-App-AN/index.html"
+  },
+  {
+    "revision": "812a4f618b1a740c3efe",
+    "url": "/Movie-App-AN/static/css/main.491ec7a2.chunk.css"
+  },
+  {
+    "revision": "6e3b4093e2806f0ff430",
+    "url": "/Movie-App-AN/static/js/2.18401eb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-App-AN/static/js/2.18401eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812a4f618b1a740c3efe",
+    "url": "/Movie-App-AN/static/js/main.a6acbb1c.chunk.js"
+  },
+  {
+    "revision": "9d565adc2c0a5a5305b0",
+    "url": "/Movie-App-AN/static/js/runtime-main.8f3b1918.js"
+  }
+]);
